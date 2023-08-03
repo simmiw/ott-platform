a2853477125cea8e64641c9cc4eeae920bbe43d6
Simmi Walia <simmiw1994@gmail.com>
2023-08-03 23:56:51 +0200

    Adding progress to the login screen

--- src/component/login/login.js
+++ src/component/login/login.js
@@ -7,22 +7,21 @@ const Login = () => {
       <Header />
       <div class="main-container">
         <div clasName="login-container">
-          <span className="heading-text">Login to countiune</span>
+          <div className="heading">
+          <span className="heading-text">Login to continue</span>
           <button type="button" className="email-or-fb-signin">
             Have a Facebook/Email account?
           </button>
+          </div>
           <div className="or-div">or</div>
-          <div className="phone-number-container">
-            <div>
-              <span className="country-code">+ 91</span>
-              <input
-                type="text"
-                tabIndex
-                pattern="[0-9]*"
-                inputMode="numeric"
-                placeholder="Enter your mobile number"
-              />
-            </div>
+          <div className="phone-input-container">
+            <span className="country-code">+91</span>
+            <input
+              className="login-input"
+              type="text"
+              inputMode="numeric"
+              placeholder="Enter your mobile number"
+            />
           </div>
         </div>
       </div>
