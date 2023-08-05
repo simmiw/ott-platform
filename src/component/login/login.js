33c08a6093f716cc39fdb8305026f5239999c461
Simmi Walia <simmiw1994@gmail.com>
2023-08-05 22:57:58 +0200

    Added clear input number functionality

--- src/component/login/login.js
+++ src/component/login/login.js
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.scss";
 import Header from "../header/header";
+import { ReactComponent as LeftArrowIcon } from "../../icons/left-arrow.svg";
 const Login = () => {
+  const [mobileNumber, setMobileNumber] = useState("");
+
+  const onMobileNumberChange = (event) => {
+    setMobileNumber(event.target.value);
+  };
+  const clearState = () => {
+    setMobileNumber("");
+  };
+
   return (
     <>
       <Header />
-      <div class="main-container">
-        <div clasName="login-container">
+      <div className="main-container">
+        <div className="login-container">
           <div className="heading">
-          <span className="heading-text">Login to continue</span>
-          <button type="button" className="email-or-fb-signin">
-            Have a Facebook/Email account?
-          </button>
+            <div>
+              {mobileNumber ? (
+                <button className="clear-button" onClick={clearState}>
+                  <LeftArrowIcon alt="left-arrow" />
+                </button>
+              ) : null}
+            </div>
+            <span className="heading-text">
+              {mobileNumber ? "Continue using phone" : "Login to continue"}
+            </span>
+            {mobileNumber ? null : (
+              <button type="button" className="email-or-fb-signin">
+                Have a Facebook/Email account?
+              </button>
+            )}
           </div>
-          <div className="or-div">or</div>
+          {mobileNumber ? null : <div className="or-div">or</div>}
           <div className="phone-input-container">
             <span className="country-code">+91</span>
             <input
+              value={mobileNumber}
               className="login-input"
               type="text"
               inputMode="numeric"
               placeholder="Enter your mobile number"
+              onChange={onMobileNumberChange}
             />
           </div>
+          {mobileNumber ? (
+            <button className="submit-button">CONTINUE</button>
+          ) : null}
         </div>
       </div>
     </>
