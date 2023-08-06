ea803afa92237bbec029daf4b46cef04781655bf
Simmi Walia <simmiw1994@gmail.com>
2023-08-06 23:32:21 +0200

    Added functionality for login via email

--- src/component/login/login.js
+++ src/component/login/login.js
@@ -4,13 +4,24 @@ import Header from "../header/header";
 import { ReactComponent as LeftArrowIcon } from "../../icons/left-arrow.svg";
 const Login = () => {
   const [mobileNumber, setMobileNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const[emailButton, setEmailButton]= useState("")
 
   const onMobileNumberChange = (event) => {
     if (event.target.value.length <= 10) setMobileNumber(event.target.value);
   };
   const clearState = () => {
     setMobileNumber("");
+    setEmail("");
   };
+
+  const loginViaEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const emailOrFBLogin =(event)=>{
+    setEmailButton(event.target)
+
+  }
 
   return (
     <>
@@ -19,20 +30,43 @@ const Login = () => {
         <div className="login-container">
           <div className="heading">
             <div>
-              {mobileNumber ? (
+              {mobileNumber ||emailButton ? (
                 <button className="clear-button" onClick={clearState}>
                   <LeftArrowIcon alt="left-arrow" />
                 </button>
               ) : null}
+
             </div>
             <span className="heading-text">
               {mobileNumber ? "Continue using phone" : "Login to continue"}
+              {emailButton
+                ? "Have an Email or Facebook account?"
+                : null}
             </span>
-            {mobileNumber ? null : (
-              <button type="button" className="email-or-fb-signin">
+            {mobileNumber || emailButton ? null : (
+              <button
+                onClick={emailOrFBLogin }
+                type="button"
+                className="email-or-fb-signin">
                 Have a Facebook/Email account?
               </button>
             )}
+            <div className="input-login-container">
+              <span className="input-login-text ">
+                Have an Email or Facebook account?
+              </span>
+              <div className="email-container">
+              {emailButton? (
+                <input
+                value={email}
+                  type="text"
+                  className="email-input"
+                  placeholder="Enter your email"
+                  onChange={loginViaEmail}
+                />
+              ) : null}
+              </div>
+            </div>
           </div>
           {mobileNumber ? null : <div className="or-div">or</div>}
           <div className="phone-input-container">
@@ -46,7 +80,7 @@ const Login = () => {
               onChange={onMobileNumberChange}
             />
           </div>
-          {mobileNumber ? (
+          {mobileNumber && emailButton ? (
             <button className="submit-button">CONTINUE</button>
           ) : null}
         </div>
